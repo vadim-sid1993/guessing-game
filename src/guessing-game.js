b64e0d88e32ca6c8c6b1c43b2d8068d0472f1203
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2022-08-14 17:37:39 +0300

    init: create task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+     this.a = min;
+     this.b = max;
     }
 
     guess() {
-
+          this.mid = Math.round((this.a+this.b)/2);
+          return this.mid;
     }
 
-    lower() {
-
+    lower(){
+        this.b = this.mid;
     }
 
     greater() {
-
+       this.a = this.mid;
     }
 }
 
